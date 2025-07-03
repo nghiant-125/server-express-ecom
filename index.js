e9680c69d6d031b9eef9c8f061ce3cfe115ab754
nghiant-125 <nghiant.125010122033@vtc.edu.vn>
2025-07-03 19:12:49 +0700

    feat: api create product & get list product

--- index.js
+++ index.js
@@ -1,16 +1,55 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
 const app = express();
+
+const mongoURL = "mongodb://14.225.198.168:27017/k23-ecom-db";
+mongoose.connect(mongoURL).then(() => {
+    console.log("MongoDB connected");
+  }).catch(err => {
+    console.error("error", err);
+  });
+
+const schemaProduct = new mongoose.Schema({
+    name: String,
+    price: Number,
+    description: String
+});
+
+const Product = mongoose.model('Product', schemaProduct);
 
 app.use(bodyParser.json());
 app.use(cors());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-    res.send('Welcome to API');
+app.get("/", (req, res) => {
+  res.send("Welcome to API");
+});
+
+app.get("/products", async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.json(products)
+  } catch (error) {
+    console.error("Error fetching product:", error);
+    res.status(500).json({message: 'Internal Server Error'});
+  }
+});
+
+app.post("/products", async (req, res) => {
+    const { name, price, description } = req.body;
+    try{
+        const newProduct = new Product({ name, price, description });
+        await newProduct.save;
+        res.status(201).json(newProduct);
+    }
+    catch (error) {
+        console.error("Error creating product:", error);
+        res.status(500).json({message: 'Internal Server Error'});
+    }
 });
 
 app.listen(5050, () => {
-    console.log('Server is running on port 5050')
+  console.log("Server is running on port 5050");
 });
